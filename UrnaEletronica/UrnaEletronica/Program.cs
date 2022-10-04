64e48331469cd82044ad616a42a57bba8037658f
GuillermeCunha <guilhermecunha1708@gmail.com>
2022-10-04 18:54:08 -0300

    Melhoria no Menu!

--- UrnaEletronica/UrnaEletronica/Program.cs
+++ UrnaEletronica/UrnaEletronica/Program.cs
@@ -20,41 +20,50 @@ namespace Urna
                 Console.WriteLine("[5] Resultado de cada candidato");
                 Console.WriteLine("[6] Resultado dos vencedores");
                 Console.WriteLine("[7] Encerrar programa! ");
-
+                Console.WriteLine("");
+                Console.WriteLine("Digite a opção desejada: ");
                 opc = int.Parse(Console.ReadLine());
 
-                switch (opc)
+                if (opc> 7 || opc < 1)
                 {
-                    case 1:
-                        Console.WriteLine("OP1");
-                        break;
-
-                    case 2:
-                        CadastroDeCandidatos.CadastroCandidato();
-                        break;
-
-                    case 3:
-                        Console.WriteLine("OP3");
-                        break;
-
-                    case 4:
-                        Console.WriteLine("OP4");
-                        break;
-
-                    case 5:
-                        Console.WriteLine("OP5");
-                        break;
-
-                    case 6:
-                        Console.WriteLine("OP6");
-                        break;
-
-                    default:
-                        Console.Clear();
-                        Console.WriteLine("Impossivel realizar outra tarefa antes de cadastrar os candidatos! ");
-                        Console.WriteLine("");
-                        break;
+                    Console.Clear();
+                    Console.WriteLine("Opção invalida! ");
                 }
+                else
+                {
+                    switch (opc)
+                    {
+                        case 1:
+                            Console.WriteLine("OP1");
+                            break;
+
+                        case 2:
+                            CadastroDeCandidatos.CadastroCandidato();
+                            break;
+
+                        case 3:
+                            Console.WriteLine("OP3");
+                            break;
+
+                        case 4:
+                            Console.WriteLine("OP4");
+                            break;
+
+                        case 5:
+                            Console.WriteLine("OP5");
+                            break;
+
+                        case 6:
+                            Console.WriteLine("OP6");
+                            break;
+
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("Impossivel realizar outra tarefa antes de cadastrar os candidatos! ");
+                            Console.WriteLine("");
+                            break;
+                    }
+                }  
             }
         }
     }
